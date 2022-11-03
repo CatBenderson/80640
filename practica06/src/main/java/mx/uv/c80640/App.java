ef408addbaa822c17aa65ac3d7a46733cac1fd7b
CatBenderson <ameyalli030@gmail.com>
2022-11-03 16:14:04 -0600

    axios

--- practica06/src/main/java/mx/uv/c80640/App.java
+++ practica06/src/main/java/mx/uv/c80640/App.java
@@ -12,6 +12,20 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+
         get("/hello", (req, res) -> "Hello World");
         get("/eduardo", (req, res) -> "Hello Eduardo");
         get("/alex", (req, res) -> "Hello Alex");
